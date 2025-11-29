2d25ddb8f9d06efe99ac2659861b7f4d19f04b58
Astat210 <alexandrkim876@gmail.com>
2025-11-29 13:31:49 +0500

    Final check: update tet

--- my-cicd-app/server.js
+++ my-cicd-app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Проверка номер 1');
+  res.send('Проверка номер 21');
 });
 
 app.listen(port, () => {
